ba1c1036459b91c444abe573fed08bdab5cf2436
marmelendez <lizbethmelendez@live.com>
2022-11-02 13:27:54 -0600

    Create experience section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from './components/Header'
 import Landing from './components/Landing'
 import Title from './components/Title'
+import Experience from './components/Experience'
 import AboutMePng from './img/about-me.png'
 import AboutMeWebp from './img/about-me.webp'
 import LinkedInPng from './img/logo-linkedin.png'
 import LinkedInWebp from './img/logo-linkedin.webp'
-import Experience from './components/Experience'
+import BoschPng from './img/logo-bosch.png'
+import BoschWebp from './img/logo-bosch.webp'
+import WokoPng from './img/logo-woko.png'
+import WokoWebp from './img/logo-woko.webp'
 // import BGVideo from './img/banner-landing.mp4'
 
 function App() {
@@ -22,7 +26,36 @@ function App() {
       Suspendisse venenatis enim ac mattis maximus.
       Quisque vestibulum tellus vel libero pulvinar auctor.
       Morbi at erat porta, scelerisque urna nec, tincidunt dui.`,
-      location: "San Francisco, CA"
+      location: "San Francisco, CA",
+      open: true
+    },
+    {
+      company: "Woko",
+      position: "Software Engineer Intern",
+      date: "Oct-Dec 2022",
+      imgPng: WokoPng,
+      imgWebp: WokoWebp,
+      description: `Phasellus laoreet ante non eleifend mollis.
+      Nullam egestas lectus ac scelerisque sollicitudin.
+      Suspendisse venenatis enim ac mattis maximus.
+      Quisque vestibulum tellus vel libero pulvinar auctor.
+      Morbi at erat porta, scelerisque urna nec, tincidunt dui.`,
+      location: "Monterrey, NL",
+      open: false
+    },
+    {
+      company: "Bosch",
+      position: "Software Engineer Intern",
+      date: "Oct 2021 - Apr 2022",
+      imgPng: BoschPng,
+      imgWebp: BoschWebp,
+      description: `Phasellus laoreet ante non eleifend mollis.
+      Nullam egestas lectus ac scelerisque sollicitudin.
+      Suspendisse venenatis enim ac mattis maximus.
+      Quisque vestibulum tellus vel libero pulvinar auctor.
+      Morbi at erat porta, scelerisque urna nec, tincidunt dui.`,
+      location: "Guadalajara, JA",
+      open: false
     }
   ]
 
@@ -51,13 +84,9 @@ function App() {
 
       <section className='section' id="experience">
         <Title number="01" sectionTitle="Experience" />
-        <Experience company={experience[0].company}
-                    position={experience[0].position}
-                    date={experience[0].date}
-                    imgWebp={experience[0].imgWebp}
-                    imgPng={experience[0].imgPng}
-                    description={experience[0].description}
-                    location={experience[0].location}/>
+        <Experience data={experience[0]}/>
+        <Experience data={experience[1]}/>
+        <Experience data={experience[2]}/>
 
       </section>
 
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,10 +1,9 @@
 import LocationIconPng from '../img/icon-location.png'
 import LocationIconWebp from '../img/icon-location.webp'
 
-function Experience(props) {
-
-    const topID = `experience-top-${props.company}`
-    const bottomID = `experience-bottom-${props.company}`
+function Experience({data}) {
+    const topID = `experience-top-${data.company}`
+    const bottomID = `experience-bottom-${data.company}`
 
     const showDescription = () => {
         const experienceTop = document.getElementById(topID)
@@ -20,35 +19,67 @@ function Experience(props) {
         }
     }
     
-    return (
-        <div className='experience'>
-            <div className='experience-top' onClick={showDescription} id={topID}>
-                <h2 className='m-0'>{props.company}</h2>
-                <p className='m-0'>{props.date}</p>
-                <h3 className='m-0 f-inter-light'>{props.position}</h3>
-            </div>
-
-            <div className='experience-bottom' id={bottomID}>
-                <picture>
-                    <source srcSet={props.imgWebp} type="image/webp" />
-                    <source srcSet={props.imgPng} type="image/png" />
-                    <img src={props.imgPng} alt="Company Logo" className='experience-img'/>
-                </picture>
-
-                <p>{props.description}</p>
-
-                <div className='experience-location'>
-                <picture>
-                    <source srcSet={LocationIconWebp} type="image/webp" />
-                    <source srcSet={LocationIconPng} type="image/png" />
-                    <img src={LocationIconPng} alt="Location icon" className="icon" />
-                </picture>
-
-                <p>{props.location}</p>
+    if (data.open) {
+        return (
+            <div className='experience'>
+                <div className='experience-top close' onClick={showDescription} id={topID}>
+                    <h2 className='m-0'>{data.company}</h2>
+                    <p className='m-0'>{data.date}</p>
+                    <h3 className='m-0 f-inter-light'>{data.position}</h3>
+                </div>
+    
+                <div className='experience-bottom' id={bottomID}>
+                    <picture>
+                        <source srcSet={data.imgWebp} type="image/webp" />
+                        <source srcSet={data.imgPng} type="image/png" />
+                        <img src={data.imgPng} alt="Company Logo" className='experience-img'/>
+                    </picture>
+    
+                    <p>{data.description}</p>
+    
+                    <div className='experience-location'>
+                    <picture>
+                        <source srcSet={LocationIconWebp} type="image/webp" />
+                        <source srcSet={LocationIconPng} type="image/png" />
+                        <img src={LocationIconPng} alt="Location icon" className="icon" />
+                    </picture>
+    
+                    <p>{data.location}</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return (
+            <div className='experience'>
+                <div className='experience-top close' onClick={showDescription} id={topID}>
+                    <h2 className='m-0'>{data.company}</h2>
+                    <p className='m-0'>{data.date}</p>
+                    <h3 className='m-0 f-inter-light'>{data.position}</h3>
+                </div>
+    
+                <div className='experience-bottom hide' id={bottomID}>
+                    <picture>
+                        <source srcSet={data.imgWebp} type="image/webp" />
+                        <source srcSet={data.imgPng} type="image/png" />
+                        <img src={data.imgPng} alt="Company Logo" className='experience-img'/>
+                    </picture>
+    
+                    <p>{data.description}</p>
+    
+                    <div className='experience-location'>
+                    <picture>
+                        <source srcSet={LocationIconWebp} type="image/webp" />
+                        <source srcSet={LocationIconPng} type="image/png" />
+                        <img src={LocationIconPng} alt="Location icon" className="icon" />
+                    </picture>
+    
+                    <p>{data.location}</p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Experience;
